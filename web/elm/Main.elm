7dfe489328c8b8c9b342b4acaa49c3a9ce9e362a
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-06 10:12:20 +0800

    modify Main to use program and build elm arch

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -4,8 +4,23 @@
 module Main exposing (..)
 
 import Html exposing (Html, text)
+import Commands exposing (fetch)
+import Messages exposing (..)
+import Model exposing (..)
+import Update exposing (..)
+import View exposing (..)
 
 
-main : Html a
+main : Program Never Model Msg
 main =
-    text "Hello, World!"
+    Html.Program
+        { init = init
+        , view = view
+        , update = update
+        , subscription = always <| Sub.none
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    initialModel ! [ fetch ]
