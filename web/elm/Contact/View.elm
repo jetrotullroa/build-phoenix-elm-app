3dc0044be1d8aa581b243473d8e8c72a625a284f
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-03-06 14:18:56 +0800

    edit contact view

--- web/elm/Contact/View.elm
+++ web/elm/Contact/View.elm
@@ -19,25 +19,29 @@ contactView model =
         fullName =
             model.first_name ++ " " ++ model.last_name
     in
-        div
-            [ classes ]
-            [ div
-                [ class "inner" ]
-                [ header []
-                    [ div [ class "avatar-wrapper" ] [ img [ class "avatar", src model.picture ] [] ]
-                    , div [ class "info-wrapper" ] [ h4 [] [ text fullName ] ]
-                    , ul
-                        [ class "meta" ]
-                        [ li [] [ i [ class "fa fa-map-marker" ] [ text model.location ] ]
-                        , li [] [ i [ class "fa fa-birthday-cake" ] [ text model.birth_date ] ]
-                        ]
-                    , div [ class "card-body" ]
-                        [ div
-                            [ class "headline" ]
-                            [ p [] [ text model.headline ] ]
-                        , ul [ class "contact-info" ]
-                            [ li [] [ i [ class "fa fa-phone" ] [ text model.phone_number ] ]
-                            , li [] [ i [ class "fa fa-envelope" ] [ text model.email ] ]
+        div [ class "row" ]
+            [ div [ class "col s12 m2" ]
+                [ div
+                    [ classes ]
+                    [ div
+                        [ class "card-content white-text" ]
+                        [ header []
+                            [ div [ class "avatar-wrapper" ] [ img [ class "avatar", src model.picture ] [] ]
+                            , div [ class "info-wrapper" ] [ h4 [] [ text fullName ] ]
+                            , ul
+                                [ class "meta" ]
+                                [ li [] [ i [ class "fa fa-map-marker" ] [ text model.location ] ]
+                                , li [] [ i [ class "fa fa-birthday-cake" ] [ text model.birth_date ] ]
+                                ]
+                            , div [ class "card-body" ]
+                                [ div
+                                    [ class "headline" ]
+                                    [ p [] [ text model.headline ] ]
+                                , ul [ class "contact-info" ]
+                                    [ li [] [ i [ class "fa fa-phone" ] [ text model.phone_number ] ]
+                                    , li [] [ i [ class "fa fa-envelope" ] [ text model.email ] ]
+                                    ]
+                                ]
                             ]
                         ]
                     ]
